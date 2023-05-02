e45fdcffc08a4d61976e2a5e86ace02d497fdd06
carrie nye <cnye@chicagobooth.edu>
2023-05-02 17:00:58 +0000

    sign in good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
   def authenticate
-    
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+    user = User.where({ :username => un }).at(0)
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "No one by that name here"})
+    else
+      if user.authenticate(pw)
+        session.store(:user_id, user.id)
+        redirect_to("/", { :notice => "Welcome back, " + user.username + "!"})
+      else
+        redirect_to("/user_sign_in", { :alert => "Nice try"})
+      end
+
+    end
   end
   
   def toast_cookies
