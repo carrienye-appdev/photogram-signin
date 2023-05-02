e43c3c3386440b043b58775643a01f21ac6565cc
carrie nye <cnye@chicagobooth.edu>
2023-05-02 16:49:43 +0000

    git password

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,14 @@ Rails.application.routes.draw do
   # CREATE
   get("/insert_user_record", {:controller => "users", :action => "create" })
 
+  get("/user_sign_up", {:controller => "users", :action => "new_registration_form" })
+
+  get("/user_sign_out", {:controller => "users", :action => "toast_cookies" })
+
+  get("/user_sign_in", {:controller => "users", :action => "new_session_form"})
+
+  post("/verify_credentials", { :controller => "users", :action => "authenticate" })
+
   # READ
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:the_username", {:controller => "users", :action => "show"})
